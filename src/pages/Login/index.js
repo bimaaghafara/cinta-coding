c69ececffc7d36ce7dcb1714b16f473b634b4861
bimaaghafara <bimaa7@gmail.com>
2022-06-10 22:50:02 +0700

    login page - initial page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,18 @@
+import { Box, Typography, Stack, Button } from '@mui/material';
+import { sx } from './styles';
+
 export default function Login() {
   return (
-    <>Login Page</>
+    <Box sx={sx.root}>
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Typography sx={sx.cintaCoding}>Cinta Coding</Typography>
+        <Button variant="contained" sx={sx.loginButton}>Login</Button>
+      </Stack>
+      <Box sx={sx.person} component="img" src="/person.png" alt="person" />
+    </Box>
   )
 }
---
+++ src/pages/Login/styles.js
@@ -0,0 +1,26 @@
+export const sx = {
+  root: {
+    padding: '24px 10%'
+  },
+  cintaCoding: {
+    fontWeight: '600',
+    fontSize: {
+      xs: '16px',
+      sm: '18px',
+      md: '24px'
+    }
+  },
+  loginButton: {
+    borderRadius: '99px',
+    width: '99px',
+    textTransform: 'none'
+  },
+  person: {
+    marginTop: {
+      xs: '100px',
+      sm: '72px',
+      md: '48px'
+    },
+    maxWidth: '100%'
+  }
+}
