81c28191fc3da0d8248efe8d53fc282b21e45041
bimaaghafara <bimaa7@gmail.com>
2022-06-11 23:54:03 +0700

    dashboard page - search input

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,22 @@
 // components & styles
 import PageLayout from "../../components/PageLayout";
+import { Box, OutlinedInput } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { sx } from './styles';
 
 export default function Dashboard() {
   return (
     <PageLayout title="Post">
-      <>Dashboard Page</>
+      <Box sx={sx.root}>
+        <OutlinedInput
+          placeholder="Search"
+          fullWidth
+          size="small"
+          sx={sx.searchInput}
+          endAdornment={<SearchIcon />}
+        />
+        <Box fullWidth>Dashboard Page</Box>
+      </Box>
     </PageLayout>
   )
 }
---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,22 @@
+export const sx = {
+  root: {
+    margin: 'auto',
+    marginTop: '48px',
+    width: '100%',
+    maxWidth: '450px',
+    '& > *': {
+      padding: '8px'
+    }
+  },
+  searchInput: {
+    margin: '24px 0',
+    background: '#EEEEF1',
+    borderRadius: '99px',
+    '& input': {
+      textAlign: 'center'
+    },
+    '& fieldset': {
+      borderRadius: '99px'
+    }
+  }
+}
